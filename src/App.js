2931a9d6618f64ef75fcf3bf5df767e0e80ea43c
Tom Coleman <tom@thesnail.org>
2016-09-07 14:00:31 +1000

    Version 6: add a vote button next to each post

--- src/App.js
+++ src/App.js
@@ -1,91 +1,10 @@
 import React, { Component } from 'react';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import { graphql, ApolloProvider } from 'react-apollo';
-import gql from 'graphql-tag';
+import { ApolloProvider } from 'react-apollo';
 
 import './App.css';
 
-class PostUpvoter extends Component {
-  constructor() {
-    super();
-    this.state = { postId: false };
-  }
-  onPostIdChanged(event) {
-    this.setState({ postId: event.target.value });
-  }
-  onUpvotePost(event) {
-    event.preventDefault();
-    this.props.mutate({ variables: { postId: this.state.postId } });
-  }
-  render() {
-    const { posts } = this.props;
-
-    return (
-      <form onSubmit={this.onUpvotePost.bind(this)}>
-        <select onChange={this.onPostIdChanged.bind(this)} value={this.state.postId}>
-          <option value={false} disabled>Select a post</option>
-          {posts.map(post =>
-            <option key={post.id} value={post.id}>{post.title}</option>
-          )}
-        </select>
-        <button disabled={!this.state.postId}>Upvote!</button>
-      </form>
-    )
-  }
-}
-
-const upvotePost = gql`
-  mutation upvotePost($postId: Int!) {
-    upvotePost(postId: $postId) {
-      id
-      votes
-    }
-  }
-`;
-const PostUpvoterWithMutation = graphql(upvotePost)(PostUpvoter);
-
-function PostList({ loading, posts }) {
-  if (loading) {
-    return <div>Loading</div>;
-  } else {
-    return (
-      <div>
-        <ul>
-          {posts.map(post =>
-            <li key={post.id}>
-              {post.title} by {' '}
-              {post.author.firstName} {post.author.lastName} {' '}
-              ({post.votes} votes)
-            </li>
-          )}
-        </ul>
-        <PostUpvoterWithMutation posts={posts} />
-      </div>
-    );
-  }
-}
-
-const allPosts = gql`
-  query allPosts {
-    posts {
-      id
-      title
-      votes
-      author {
-        id
-        firstName
-        lastName
-      }
-    }
-  }
-`
-
-const PostListWithData = graphql(allPosts, {
-  props: ({data: { loading, posts }}) => ({
-    loading,
-    posts,
-  }),
-})(PostList);
+import PostList from './PostList';
 
 class App extends Component {
   constructor(...args) {
@@ -100,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={this.client}>
-        <PostListWithData />
+        <PostList />
       </ApolloProvider>
     );
   }
