4fb66e18f6b0c7ba1126f2c0181dee21c9e68f8c
Tom Coleman <tom@thesnail.org>
2016-09-15 11:37:44 +1000

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,14 @@
 sed -i '' /PostUpvoter/d src/PostList.js
 sed -i '' s/stepX/step1/ package.json
 npm run build
-cd build;
-aws s3 sync . s3://dev.apollodata.com/frontpage-react-app-step1 --acl public-read
+(cd build;
+aws s3 sync . s3://dev.apollodata.com/frontpage-react-app-step1 --acl public-read)
 
 # clean up
 git reset --hard
 sed -i '' s/stepX/step2/ package.json
 npm run build
-aws s3 sync . s3://dev.apollodata.com/frontpage-react-app-step2 --acl public-read
+(cd build;
+aws s3 sync . s3://dev.apollodata.com/frontpage-react-app-step2 --acl public-read)
 
 git reset --hard
