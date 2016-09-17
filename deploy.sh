84e058b1a53f704b00630f5f49f04c1ed2eec379
Tom Coleman <tom@thesnail.org>
2016-09-17 14:19:00 +1000

    Rename deployed app

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 
 # remove references to PostUpvoter from PostList
 sed -i '' /PostUpvoter/d src/PostList.js
-sed -i '' s/stepX/step1/ package.json
+sed -i '' s/operation/query/ package.json
 npm run build
 (cd build;
-aws s3 sync . s3://dev.apollodata.com/frontpage-react-app-step1 --acl public-read)
+aws s3 sync . s3://dev.apollodata.com/frontpage-react-app-query --acl public-read)
 
 # clean up
 git reset --hard
-sed -i '' s/stepX/step2/ package.json
+sed -i '' s/operation/mutation/ package.json
 npm run build
 (cd build;
-aws s3 sync . s3://dev.apollodata.com/frontpage-react-app-step2 --acl public-read)
+aws s3 sync . s3://dev.apollodata.com/frontpage-react-app-mutation --acl public-read)
 
 git reset --hard
